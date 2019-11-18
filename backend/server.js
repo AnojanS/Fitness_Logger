3965e577950a8a5994bb67ffdc9cd772e07c798b
AnojanS <asaba091@uottawa.ca>
2019-11-17 21:09:50 -0500

    connected to mongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //establishing dependency requirements
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 //configuring environment variables into dotenv file
 require('dotenv').config(); 
@@ -8,9 +9,17 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//allowing the parsing of JSON in and out of server
+//allowing JSON parsing in and out of server
 app.use(cors());
 app.use(express.json());
+
+//connecting to mongoDB database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("successfully connected to MongoDB database");
+})
 
 //starting server
 app.listen(port, () => {
